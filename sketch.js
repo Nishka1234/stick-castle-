76c4a01d9206a25179de320a878438c1e9157bb5
Nishka1234 <56813389+Nishka1234@users.noreply.github.com>
2020-01-26 10:22:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("black");  
+  fill("brown")
+rect(300,200,20,200)
+rect(280,220,20,180)
+rect(260,200,20,200)
+rect(240,220,20,180)
+rect(220,200,20,200)
+rect(420,200,20,200)
+rect(440,220,20,180)
+rect(460,200,20,200)
+rect(480,220,20,180)
+rect(500,200,20,200)
+fill("pink")
+rect(280,260,20,20)
+rect(240,260,20,20)
+rect(440,260,20,20)
+rect(480,260,20,20)
+rect(320,100,20,350)
+rect(340,120,20,330)
+rect(360,100,20,350)
+rect(380,120,20,330)
+rect(400,100,20,350)
+fill("blue")
+rect(340,200,20,20)
+rect(380,200,20,20)
+fill("white")
+rect(370,50,2,50)
+fill("green")
+rect(370,50,30,20)
+fill("orange")
+rect(370,50,30,7)
+fill("white")
+rect(370,55,30,7)
+text("HAPPY REPUBLIC DAY",600,100)
+  drawSprites();
+}
